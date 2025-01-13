42a6e8df86b97e3e9c0027c290bb7f720603bd71
N3rd <landonmarting@icloud.com>
2025-01-12 19:56:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Create a button element
+const button = document.createElement('button');
+
+// Set the button's text
+button.innerText = 'Click Me!';
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    alert('Button was clicked!');
+});
+
+// Append the button to the body (or any other element)
+document.body.appendChild(button);
+
 // Create the main container
 const container = document.createElement('div');
 container.style.width = '300px';
